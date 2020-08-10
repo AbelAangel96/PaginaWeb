14f2bd96d2f632b0aa7d2f373808cd617ab930f9
Abel Angel <abel.angel1996@gmail.com>
2020-08-09 23:10:57 -0300

    seccion nosotros en progreso

--- home2/header.js
+++ home2/header.js
@@ -3,8 +3,11 @@ $(document).ready(function(){
     $(window).scroll(function(){
         if ($(this).scrollTop() > 0 ){
             $('header').addClass('header2')
+            $('section').addClass('padding')
         } else {
             $('header').removeClass('header2');
+            $('section').removeClass('padding')
         }
     });
 });
+
